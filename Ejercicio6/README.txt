259164dc2f97918bc16e831ce548c5f4e5ad9772
juliolopez615 <juliolopeznaranjo@gmail.com>
2022-03-14 19:00:45 +0100

    Añadida otra calculadora algo mas compleja+README

---
+++ Ejercicio6/README.txt
@@ -0,0 +1,3 @@
+Con el fin de manejar mejor Js, he realizado una calculador un poco más complicada a
+parte de los ejercicios 6.1 y 6.2. Lo he llamado 6.3. Es simple y su única función era
+la de manejarme mejor con Js, Css y Html :).
--- Ejercicio6/ejercicio6.js
+++ Ejercicio6/ejercicio6.js
@@ -1,4 +1,5 @@
 array = [];
+op = "";
 function agregarNumero(){
     if (isNaN(campo.value) || campo.value == "") {
         alert("Por favor, introduce un numero.")
@@ -35,5 +36,57 @@ function operacion(id){
           console.log("Algo ha ido mal")
           break;
       }
+}
+
+function presionar(id){
+    if(document.getElementById(id).value == '+' || document.getElementById(id).value == '-' || document.getElementById(id).value == '*' || document.getElementById(id).value == '/' || document.getElementById(id).value == '%'){
+        
+        if(op != ""){
+            alert("Esta calculadora solo permite una operacion :(")
+            return
+        }
+        else{
+            op = document.getElementById(id).value
+        }
+        
+    }
+
+    if(document.getElementById(id).value != 'igual' && document.getElementById(id).value != 'C' ){
+        texto.value += document.getElementById(id).value
+    }
+    else{
+        if(document.getElementById(id).value == 'C'){texto.value=""}
+        else{
+            let resultado
+            switch (op) {
+                case '+':
+                    resultado = texto.value.split('+')
+                    texto.value = parseInt(resultado[0]) + parseInt(resultado[1])
+                    break;
+                case '-':
+                    resultado = texto.value.split('-')
+                    texto.value = parseInt(resultado[0]) - parseInt(resultado[1])
+                    break;
+                case '*':
+                    resultado = texto.value.split('*')
+                    texto.value = parseInt(resultado[0]) * parseInt(resultado[1])
+                    break;
+                case '/':
+                    resultado = texto.value.split('/')
+                    texto.value = (parseInt(resultado[0]) / parseInt(resultado[1])).toFixed(2)
+                    break;
+                case '%':
+                    resultado = texto.value.split('%')
+                    texto.value = parseInt(resultado[0]) % parseInt(resultado[1])
+                    break;
+                default:
+                console.log("Algo ha ido mal")
+                break;
+            }
+          op = "";  
+
+        }
+              
+    } 
 }
 
