5840fa1d331f89fa4dbacae8d858a676fba84421
juliolopez615 <juliolopeznaranjo@gmail.com>
2022-03-16 13:11:53 +0100

    Añadido Ejercicio 9 a la carpeta de Angular

--- Angular/Ejercicio9/README.md
+++ Angular/Ejercicio9/README.md
@@ -1,4 +1,4 @@
-# Proyecto1
+# Ejercicio9
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.6.
 
---
+++ Angular/Ejercicio9/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre = "Julio López Naranjo"
+  titulo = "Ingeniero de Software"
+  titulo1 = 'Perfil personal'
+  titulo2 = 'Datos de contacto'
+  titulo3 = 'Destrezas'
+  titulo4 = 'Conocimientos de Software'
+  titulo5 = 'Experiencia laboral'
+  titulo6 = 'Formación'
+  descripcion = "Ingeniero en formación. Trabajador y comprometido. Con muchas ganas de adquirir experiencia y conocimientos."
+  descripcionEmpleo = "Colaboraba tanto en la subida, bajada o eliminación de productos de la empresa como también en alguna labor de mantenimiento de la web."
+  current = "Cursando 4º curso"
+}
+
+
